39d23d0993ed35c4c683f6daa7e25ccae2dbbc9e
Mark April L Buit <zodiacmark@gmail.com>
2016-12-13 09:18:14 +0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+main(){
+   int a, b, c, d;
+   printf("enter value of a: ");
+   scanf("%d", &a);
+   printf("enter value of b: ");
+   scanf("%d", &b);
+   printf("enter value of c ");
+   scanf("%d", &c);
+	printf("%d", d= (a+b+c)/3);
+   
+}
